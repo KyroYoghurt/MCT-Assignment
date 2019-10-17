ad3cc01f7bc6139d657e72830b29a438006ec250
manishdash123 <31148348+manishdash123@users.noreply.github.com>
2019-10-18 00:14:54 +0530

    Update convert2Text.m

--- convert2Text.m
+++ convert2Text.m
@@ -4,7 +4,7 @@ nchar = l/7
 x = char(zeros(1,nchar))
 k =1;
 for i = 1:7:l;
-    c = native2unicode(bin2dec(bin(1,i:i+6)));
+    c = native2unicode(bin2dec(bitstream(1,i:i+6)));
     x(k)=c;
     k = k+1;
 end
