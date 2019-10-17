01858854425791232f1b1f1d0bcaa6795acbaf3c
manishdash123 <31148348+manishdash123@users.noreply.github.com>
2019-10-17 23:46:14 +0530

    Update convert2Text.m

--- convert2Text.m
+++ convert2Text.m
@@ -3,10 +3,10 @@ l = size(bitstream,2)
 nchar = l/7
 x = char(zeros(1,nchar))
 k =1;
-for i = 1:7:l
-    c = native2unicode([64 32 16 8 4 2 1]*((bitstream(i:i+6)-48)'));
-    x(k) = c;
-    k=k+1;
+for i = 1:7:l;
+    c = native2unicode(bin2dec(bin(1,i:i+6)));
+    x(k)=c;
+    k = k+1;
 end
 end
 
